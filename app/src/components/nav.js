0ffaa88df9c774a48c343acfb15bc809f5724081
Trai Lynne <trailynne@gmail.com>
2019-09-25 15:09:59 -0400

    Create Nav Component Add logo as file Update favicon Render nav in App.js Add fonts and icon access to index.html

---
+++ app/src/components/nav.js
@@ -0,0 +1,74 @@
+import React, {
+    Component
+} from 'react';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+import logo from '../assets/img/logo.PNG'
+
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="https://www.trailynne.org"><img src={logo} style={{height: 30}} alt='Trai Lynne Logo'/> Trai Lynne</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>Projects</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Skills</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='https://www.trailynne.org/blog'>Blog</NavLink>
+              </NavItem>
+            </Nav>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href='mailto:trailynne@gmail.com' target='_blank'><i class="fas fa-envelope-open-text"></i></NavLink>
+              </NavItem>
+              <NavItem href=''>
+                <NavLink href='https://www.linkedin.com/in/trai-lynne-compton/' target='_blank'><i class="fab fa-linkedin-in"></i></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='https://github.com/trailynne' target='_blank'><i class="fab fa-github"></i></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='https://www.instagram.com/trailynne/' target='_blank'><i class="fab fa-instagram"></i></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href='https://twitter.com/trailynne/' target='_blank'><i class="fab fa-twitter"></i></NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
