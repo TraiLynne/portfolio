62d962a3d14bd183d874edd3a56ed815b9186f7e
Trai Lynne <trailynne@gmail.com>
2019-09-25 22:46:11 -0400

    Updates

---
+++ precache-manifest.9a64bdf78d0d594902acfc4289fd6066.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b545e1d5928f1f60a52544833bd7b8a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "84d3df8df3eeecf0c5f2",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "37f948fdcc71eaef8b0e",
+    "url": "/portfolio/static/css/main.df6f5567.chunk.css"
+  },
+  {
+    "revision": "84d3df8df3eeecf0c5f2",
+    "url": "/portfolio/static/js/2.fdb94426.chunk.js"
+  },
+  {
+    "revision": "37f948fdcc71eaef8b0e",
+    "url": "/portfolio/static/js/main.132ef418.chunk.js"
+  },
+  {
+    "revision": "f0f989ada065c03bd958",
+    "url": "/portfolio/static/js/runtime-main.21eabe76.js"
+  },
+  {
+    "revision": "f45621d8b00da529f5418335ec963b57",
+    "url": "/portfolio/static/media/UIProject.f45621d8.png"
+  },
+  {
+    "revision": "32238388365e79eec61eeb8dff6eac2d",
+    "url": "/portfolio/static/media/backend.32238388.png"
+  },
+  {
+    "revision": "f7dcc9a049ccdea1a36b1f491cd9f755",
+    "url": "/portfolio/static/media/course-correct.f7dcc9a0.png"
+  },
+  {
+    "revision": "1524842bd518f0d61ab3944ab2ad7298",
+    "url": "/portfolio/static/media/frontend.1524842b.png"
+  },
+  {
+    "revision": "612250cf642d700f7fe7678c6a1ae565",
+    "url": "/portfolio/static/media/human.612250cf.png"
+  },
+  {
+    "revision": "d03c0b9c6ebb3cd7ab9b0d3f1dd7a2b4",
+    "url": "/portfolio/static/media/jubilee.d03c0b9c.png"
+  },
+  {
+    "revision": "d38230cf00c95fbb262318a5200214b0",
+    "url": "/portfolio/static/media/logo.d38230cf.PNG"
+  },
+  {
+    "revision": "17a67204a33941c5c66bfe6af7132adf",
+    "url": "/portfolio/static/media/symposium.17a67204.png"
+  },
+  {
+    "revision": "6a06c66959610da420869d879ded9f81",
+    "url": "/portfolio/static/media/tech.6a06c669.png"
+  }
+]);
